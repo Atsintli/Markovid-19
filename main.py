e228da685b7a91e93afe458bd438de61ab72bed1
Atsintli <atsintli@riseup.net>
2020-05-12 17:29:31 +0000

    añade segundo orden

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ leer en la ventana del lado derecho lo que ha generado el algoritmo, y del lado 
 
 ¿Cómo funciona?
 
-Las cadenas de markov deben su nombre al matemático Andréi Márkov y  parten de procesos aleatorios donde la probabilidad de que ocurra un evento depende solamente del estado inmediatamente anterior de un conjunto de valores. En las cadenas de primer orden el algoritmo toma como referencia la penúltima palabra para decidir cual le podría seguir partiendo de una selección aleatoria de palabras de acuerdo con la base de datos introducida. En cambio, las cadenas de tercer orden toman como referencia 3 palabras antes de seleccionar la siguiente. Dependiendo de la cantidad de palabras diferentes que le sigan a una palabra dada, será la probabilidad de aparición que se le asignará a las posibles palabras o grupo de palabras consecuentes.
+Las cadenas de markov deben su nombre al matemático Andréi Márkov y  parten de procesos aleatorios donde la probabilidad de que ocurra un evento depende solamente del estado inmediatamente anterior de un conjunto de valores. En las diccionarios de primer orden el algoritmo toma como referencia la penúltima palabra para decidir cual le podría seguir partiendo de una selección aleatoria de palabras de acuerdo con la base de datos introducida. En cambio, las diccionarios de tercer orden toman como referencia 3 palabras antes de seleccionar la siguiente. Dependiendo de la cantidad de palabras diferentes que le sigan a una palabra dada, será la probabilidad de aparición que se le asignará a las posibles palabras o grupo de palabras consecuentes.
 
 Referencias:
 https://setosa.io/ev/markov-chains/
@@ -32,30 +32,30 @@ texto = open('markovid_data.txt')
 palabras = []
 
 for lineas in texto:
-    lineas = lineas.replace('\r', ' ').replace('\n', ' ')
+    lineas = lineas.replace('\r', ' ').replace('\n', '')
     nuevas_palabras = lineas.split(' ')
     nuevas_palabras = [palabra for palabra in nuevas_palabras if palabra not in ['', ' ']]
     palabras = palabras + nuevas_palabras
 
-cadena = {}
+diccionario = {}
 n_palabras = len(palabras)
 
-for i, key in enumerate(palabras):
+for i, palabra in enumerate(palabras):
     if n_palabras > (i + 1):
-        palabra = palabras[i + 1]
-        if key not in cadena:
-            cadena[key] = [palabra]
+        nueva_palabra = palabras[i + 1]
+        if palabra not in diccionario:
+            diccionario[palabra] = [nueva_palabra]
         else:
-            cadena[key].append(palabra)
+            diccionario[palabra].append(nueva_palabra)
 
 w1 = random.choice(palabras)
 tweet = w1
 while len(tweet) < 350:
-    w2 = random.choice(cadena[w1])
+    w2 = random.choice(diccionario[w1])
     tweet += ' ' + w2
     w1 = w2
 
-print('Hay un Corpus de: ', str(n_palabras), ' palabras.', 'y un total de: ', str(len(cadena)), ' palabras diferentes.')
+print('Hay un Corpus de: ', str(n_palabras), ' palabras.', 'y un total de: ', str(len(diccionario)), ' palabras diferentes.')
 
 print('\n', 'Markovid-19 de primer orden')
 print('\n', tweet)
@@ -63,26 +63,54 @@ print('\n', tweet)
 salida = gTTS(text=tweet, lang=lang)
 salida.save('./markovid-19_primer_orden.wav')
 
+#### Markovid-19 de segundo orden ####
+
+diccionario = {}  
+n_words = len(palabras)  
+for i, palabra1 in enumerate(palabras):  
+    if n_words > i + 2:
+        palabra2 = palabras[i + 1]
+        nueva_palabra = palabras[i + 2]
+        if (palabra1, palabra2) not in diccionario:
+            diccionario[(palabra1, palabra2)] = [nueva_palabra]
+        else:
+            diccionario[(palabra1, palabra2)].append(nueva_palabra)
+        
+r = random.randint(0, len(palabras) - 1) 
+nueva_palabra = (palabras[r], palabras[r + 1])
+tweet = nueva_palabra[0] + ' ' + nueva_palabra[1] 
+while len(tweet) < 350:  
+    w = random.choice(diccionario[nueva_palabra])
+    tweet += (" " + w)
+    nueva_palabra = (nueva_palabra[1], w)
+
+print('\n', 'Markovid-19 de tercer orden')
+print('\n', tweet)
+
+salida = gTTS(text=tweet, lang=lang)
+salida.save('./markovid-19_segundo_orden.wav')
+
 #### Markovid-19 de tercer orden: adquiere un poco más de sentido lógico ####
 
-cadena = {}  
+diccionario = {}  
 n_words = len(palabras)  
-for i, key1 in enumerate(palabras):  
+for i, palabra in enumerate(palabras):  
     if n_words > i + 3:
-        key2 = palabras[i + 1]
-        key3 = palabras[i + 2]
-        palabra = palabras[i + 3]
-        if (key1, key2, key3) not in cadena:
-            cadena[(key1, key2, key3)] = [palabra]
+        palabra2 = palabras[i + 1]
+        palabra3 = palabras[i + 2]
+        nueva_palabra = palabras[i + 3]
+        if (palabra, palabra2, palabra3) not in diccionario:
+            diccionario[(palabra, palabra2, palabra3)] = [nueva_palabra]
         else:
-            cadena[(key1, key2, key3)].append(palabra)            
+            diccionario[(palabra, palabra2, palabra3)].append(nueva_palabra)  
+
 r = random.randint(0, len(palabras) - 1) 
-key = (palabras[r], palabras[r + 1], palabras[r + 2])
-tweet = key[0] + ' ' + key[1] + ' ' +  key[2]
-while len(tweet) < 300:  
-    w = random.choice(cadena[key])
+nueva_palabra = (palabras[r], palabras[r + 1], palabras[r + 2])
+tweet = nueva_palabra[0] + ' ' + nueva_palabra[1] + ' ' +  nueva_palabra[2]
+while len(tweet) < 350:  
+    w = random.choice(diccionario[nueva_palabra])
     tweet += (" " + w)
-    key = (key[1], key[2], w)
+    nueva_palabra = (nueva_palabra[1], nueva_palabra[2], w)
 
 print('\n', 'Markovid-19 de tercer orden')
 print('\n', tweet)
